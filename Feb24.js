a93cc1607d7519524bbfe9a250e9ffd8ff7f4edb
stacksonstack <staceycarrillo44@gmail.com>
2021-02-24 22:34:44 -0500

    third challenge done

--- Feb24.js
+++ Feb24.js
@@ -98,7 +98,6 @@ var numJewelsInStones = function (jewels, stones) {
 
 
 
-
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
