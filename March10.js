ff036c6fb5f48019e9d9cdec6a055e7ff4a43122
stacksonstack <staceycarrillo44@gmail.com>
2021-03-10 18:08:55 -0500

    second challenge done

--- March10.js
+++ March10.js
@@ -10,3 +10,13 @@ var runningSum = function(nums) {
 
 // We can buy more memory, we can't buy more time. 
 // Memory capacity is always being improved, time is always static. 
+
+
+var maximumWealth = function(accounts) {
+    var sum = (n) => n.reduce((acc, cur) => acc + cur);
+    var sumArray = []
+    accounts.forEach((account) => {
+       sumArray.push(sum(account))
+    })
+    return Math.max(...sumArray)
+};
