cd88cd535cc3f1129da98e913e5dd4204b79af28
stacksonstack <staceycarrillo44@gmail.com>
2021-04-07 23:48:44 -0400

    second challenge done

--- April7.js
+++ April7.js
@@ -17,7 +17,7 @@ var replaceElements = function (arr) {
     return array.reverse();
   };
 
-  console.log(replaceElements([17,18,5,4,6,1])) // [18,6,6,6,1,-1]
+//   console.log(replaceElements([17,18,5,4,6,1])) // [18,6,6,6,1,-1]
 
 
   var replaceElements2 = function (arr) {
@@ -30,4 +30,22 @@ var replaceElements = function (arr) {
     return array.reverse();
   };
 
-  console.log(replaceElements2([17,18,5,4,6,1])) // [18,6,6,6,1,-1]
+//   console.log(replaceElements2([17,18,5,4,6,1])) // [18,6,6,6,1,-1]
+
+
+// Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+// For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them. Notice that multiple kids can have the greatest number of candies.
+
+var kidsWithCandies = function(candies, extraCandies) {
+    let maxCandies = Math.max(...candies)
+    let output = []
+    candies.forEach((candy)=>{
+        let extraCandy = candy + extraCandies
+        extraCandy >= maxCandies ? output.push(true) : output.push(false)
+    })
+    return output
+};
+
+// console.log(kidsWithCandies([2,3,5,1,3], 3)) // [true,true,true,false,true]
+
+
