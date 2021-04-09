16b36c236527cd5081fe8571cbf0cb9e466b6c21
stacksonstack <staceycarrillo44@gmail.com>
2021-04-08 22:35:28 -0400

    first challenge done

---
+++ April8.js
@@ -0,0 +1,13 @@
+// Given an alphanumeric string s, return the second largest numerical digit that appears in s, or -1 if it does not exist.
+
+// An alphanumeric string is a string consisting of lowercase English letters and digits.
+
+var secondHighest = function(s) {
+    var array = s.split('').map(el => parseInt(el)).filter((el)=> !isNaN(el));
+    var max = Math.max(...array)
+    var maxRemoved = array.filter((el)=> el != max)
+    return maxRemoved.length == 0 ?  -1 : Math.max(...maxRemoved) 
+};
+
+console.log(secondHighest("dfa12321afd")) //2
+console.log(secondHighest("abc1111")) //-1
