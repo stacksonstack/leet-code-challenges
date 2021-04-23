0e4ae81c97c323bcb7c855323663c4b443d291ae
stacksonstack <staceycarrillo44@gmail.com>
2021-04-22 22:58:36 -0400

    second challenge done

--- April22.js
+++ April22.js
@@ -5,4 +5,14 @@ var maxProduct = function(nums) {
     return (myArray[myArray.length-1] - 1) * (myArray[myArray.length - 2] -1)
 };
 
-console.log(maxProduct([10,2,5,2]))
+// console.log(maxProduct([10,2,5,2]))
+
+// Given a list of sorted characters letters containing only lowercase letters, and given a target letter target, find the smallest element in the list that is larger than the given target.
+
+// Letters also wrap around. For example, if the target is target = 'z' and letters = ['a', 'b'], the answer is 'a'.
+
+var nextGreatestLetter = function(letters, target) {
+    return letters.find(c => c > target) || letters[0];
+  };
+
+  console.log(nextGreatestLetter(["c","f","j"], "a"))
